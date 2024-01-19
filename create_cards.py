d56c6263db65f85b715d41af4afc13084661fd32
Bladimir <oirbl@hotmail.com>
2024-01-19 09:24:31 -0600

    🎨 Split effort

--- create_cards.py
+++ create_cards.py
@@ -1,6 +1,8 @@
 from docx import Document
 from docx.shared import Pt, RGBColor
 from docx.enum.text import WD_ALIGN_PARAGRAPH
+import os
+import json
 
 def create_card(doc, text):
     # Add a table with one cell (1 row, 1 column)
@@ -13,7 +15,7 @@ def create_card(doc, text):
 
     # Add some text to the cell
     cell.paragraphs[0].paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-    cell.text = text 
+    cell.text = text.strip()
 
     font = cell.paragraphs[0].runs[0].font
     font.color.rgb = RGBColor(79, 113, 190)
@@ -25,6 +27,8 @@ def create_card(doc, text):
 
     doc.add_paragraph()
 
+translations = [] 
+
 def create_doc_file():
     doc = Document()
     # Create a card for each element in the array
@@ -32,7 +36,19 @@ def create_doc_file():
         create_card(doc, text)
 
     # Save the Word document
-    doc.save("output.docx")
+    doc.save("cards.docx")
 
+def get_data():
+    file_name = "data.json"
+    if not os.path.exists(file_name):
+        # I should automate this, maybe.
+        raise ValueError(f"{file_name} not found in root path. Make sure to run generate_data.py first.")
+    
+    with open(file_name, "r") as file:
+        data = json.load(file)
 
+    for _, value in data.items():
+        translations.append(value["spanish"])
+
+get_data()
 create_doc_file()
